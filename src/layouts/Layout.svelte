3b694a8aa4fb1b1c36c07eb3981ea7de882a1c25
macabeus <bruno.macabeus@gmail.com>
2021-02-13 06:54:19 +0000

    assets: add favicon

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -27,6 +27,11 @@
 <svelte:head>
   <link rel="stylesheet" href={`${settings.prefix}/style.css`} />
   <link rel="stylesheet" href="https://unpkg.com/balloon-css/balloon.min.css" />
+  <link rel="icon" type="image/png" sizes="16x16" href={`${settings.prefix}/favicons/16x16.png`}>
+  <link rel="icon" type="image/png" sizes="32x32" href={`${settings.prefix}/favicons/32x32.png`}>
+  <link rel="icon" type="image/png" sizes="96x96" href={`${settings.prefix}/favicons/96x96.png`}>
+  <link rel="apple-touch-icon" type="image/png" sizes="76x76" href={`${settings.prefix}/favicons/76x76.png`}>
+  <link rel="apple-touch-icon" type="image/png" sizes="120x120" href={`${settings.prefix}/favicons/120x120.png`}>
 </svelte:head>
 
 <Header settingsPrefix={settings.prefix} />
